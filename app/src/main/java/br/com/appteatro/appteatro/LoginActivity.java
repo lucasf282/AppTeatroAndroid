9085e603831bae1d9ee23794070cbe7376271580
thigo08 <thsousa8@hotmail.com>
2017-11-14 22:37:10 -0200

    Implementação Inicial do Menu Lateral.

--- app/src/main/java/br/com/appteatro/appteatro/LoginActivity.java
+++ app/src/main/java/br/com/appteatro/appteatro/LoginActivity.java
@@ -268,7 +268,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     }
 
     private void goMainScreen() {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, MenuLateralActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
     }
